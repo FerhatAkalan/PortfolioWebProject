e40831316a8b6c196d69b375ea512884a2962a6c
Ferhat Akalan <ferhatakalan4@gmail.com>
2023-03-21 15:30:22 +0300

    Proje dosyası ekle.

---
+++ Business Layer/Concrete/AboutManager.cs
@@ -0,0 +1,45 @@
+﻿using Business_Layer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About TGetById(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetList();        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
---
+++ Core_Proje/ViewCompanents/Feature/FeatureList.cs
@@ -0,0 +1,20 @@
+﻿using Business_Layer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewCompanents.Feature
+{   
+    public class FeatureList : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
